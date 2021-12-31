b49b3aab319ac3a902e297b2539464f8ee2569c5
Luis Vitor <luis.vitor@yazo.com.br>
2021-12-30 23:54:46 -0300

    improvement: use a command to get jokes

--- src/Commands/commands.ts
+++ src/Commands/commands.ts
@@ -3,6 +3,10 @@ const commands = [
     name: "ping",
     description: "Replies with Pong!",
   },
+  {
+    name: "joke",
+    description: "Replies with a joke!",
+  },
 ];
 
 export { commands };
--- src/Interactions/interactions.ts
+++ src/Interactions/interactions.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { CommandInteraction, CacheType } from "discord.js";
 
 const interactions = [
@@ -5,6 +6,22 @@ const interactions = [
     command: "ping",
     run: async (interaction: CommandInteraction<CacheType>) => {
       await interaction.reply("Pong");
+    },
+  },
+  {
+    command: "joke",
+    run: async (interaction: CommandInteraction<CacheType>) => {
+      try {
+        const response = await axios.get(
+          `https://api.chucknorris.io/jokes/random`
+        );
+
+        const { value } = response.data;
+
+        await interaction.reply(value);
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 ];
